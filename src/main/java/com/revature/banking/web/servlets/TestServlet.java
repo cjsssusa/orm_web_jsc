cdee677582bdd73fff81109a42e58aafaacf6e8f
cjsssusa <cjsssusa@gmail.com>
2021-12-01 07:53:05 -0500

    test - v4

--- src/main/java/com/revature/banking/web/servlets/TestServlet.java
+++ src/main/java/com/revature/banking/web/servlets/TestServlet.java
@@ -12,6 +12,6 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("<h1>/test works...Dario-JongSoo-v3</h1>");
+        resp.getWriter().write("<h1>/test works...Dario-JongSoo-v4</h1>");
     }
 }
